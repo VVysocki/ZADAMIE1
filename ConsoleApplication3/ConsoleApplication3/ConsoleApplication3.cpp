b5bb5c8bffc7a404d31ddadaae2be6dd6f843dd6
VVysocki <92871273+VVysocki@users.noreply.github.com>
2021-11-29 17:28:41 +0100

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,21 @@
+// ConsoleApplication3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <time.h >
+using namespace std;
+
+
+int main()
+{
+	int liczba, n, suma = 0;
+	cout << "Podaj n:", cin >> n;
+	int temp = n;
+	if (n > 0) {
+		for (int i = 0; i < n; i++)
+
+	}
+	
+}
+
+
